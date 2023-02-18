904d098658fae54c04ad7548e3c53e7c7e609408
Your Name <you@example.com>
2023-02-19 00:44:18 +0700

    UPDATE

--- LOGIN.cpp
+++ LOGIN.cpp
@@ -1,13 +1,37 @@
+
 #include<iostream>
 #include<istream>
 #include<fstream>
 #include<cstdlib>
 #include<string>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
 
+// Bookshop Class
+class bookstore {
+public:
+    void Start();
+    void Login();
+    void Register();
+    void Control_panel();
+    //void Buy_book(); 
+    //void Show_book();
+};
+
+// Function to display first page
+void bookstore::Start()
+{
+    cout << "\n";
+    cout << "-------- WELCOME TO CPE BOOKSTORE --------\n\n";
+    cout << "\t  Press < 1 > to Login  \n";
+    cout << "\t  Press < 2 > to Register  \n";
+}
+
 // Function Login
-void Login()
+void bookstore::Login()
 {
     int come;
     string name , password , n , pw;
@@ -36,11 +60,6 @@ void Login()
         cout << "\nLogin successfaul ++\n\n";
         cout << "Hello , " << name << " What do you want to do?\n";
         cin.get();
-
-        cout << "\n";
-        cout << "\tPress < 1 > to Buy a book" << endl;
-        cout << "\tPress < 2 > to Show all books" << endl;
-        cout << "\tPress < 3 > to Exit" << endl;
     }
     else
     {
@@ -51,7 +70,7 @@ void Login()
 }
 
 // Function Register
-void Register()
+void bookstore::Register()
 {
     string name , password;
     //กรอกชื่อและรหัสเพื่อสมัคร
@@ -68,36 +87,89 @@ void Register()
     dest << name << " " << password << endl;
     
     cout << "\nSuccessful register ++ \n";
-    Login(); //ให้ login ต่อเลย
+    // สมัครเสร็จเเล้ว จะไปที่หน้า first page
   
 }
 
-//Function menu
-void menu(int x){
-    
+// Function to display the menus
+void bookstore::Control_panel()
+{
+    cout << "\n";
+    cout << "\tPress < 1 > to Buy a book" << endl;
+    cout << "\tPress < 2 > to Show all books" << endl;
+    cout << "\tPress < 3 > to Exit" << endl;
+}
+
+// Function Buy book 
+// void bookstore::Buy_book(){}
+
+// Function Show all book 
+// void bookstore::Show_book(){}
+
+
+// Function for book store record
+void bookstorerecord()
+{
+	int choice_start;
+    int choice_control;
+	bookstore b;
+
+	while (1) {
+
+		b.Start();
+		cout << "\t\nWhat is your answer : ";
+		cin >> choice_start;
+		switch (choice_start) {
+
+		case 1:
+			b.Login();
+            b.Control_panel();
+            cout << "\t\nWhat is your answer : ";
+		    cin >> choice_control;
+            while (1)
+            {
+                switch (choice_control)
+                {
+                case 1:
+                    /* code buy book */
+                    cout << "buy book5555555555";
+                    exit(0);
+                    break;
+
+                case 2:
+                    /* code show book*/
+                    cout << "Show book54878899";
+                    exit(0);
+                    break;
+                
+                case 3:
+                    exit(0);
+                    break;;
+                
+                default:
+                    break;
+                }
+               
+            }
+            break;
+
+		case 2:
+			b.Register();
+            break;
+
+		default:
+			cout << "\nxxxxx INVALID CHOICE xxxxx \n";
+            exit(0);
+		}
+	}
 }
 
 
 int main()
 {
-    int choose;
-    int choose_menu;
-    cout << "\n";
-    cout << "-------- WELCOME TO CPE BOOKSTORE --------\n\n";
-    cout << "\t  Press < 1 > to Login  \n";
-    cout << "\t  Press < 2 > to Register  \n";
-    cout << "\t\nWhat is your answer : ";
-    cin >> choose;
-    if(choose == 1) Login();
-    else if(choose == 2) Register();
-    else { 
-        cout << "\t\nxxxxx ERROR xxxxx\n"; 
-        return 0; 
-    }
-    
-    cout << "\t\nWhat is your answer : ";
-    cin >> choose_menu;
-    menu(choose_menu);
+	// Function Call
+	bookstorerecord();
 
-    //เติม Function เลือก 1 2 3
-}   
+	return 0;
+}
+ 
