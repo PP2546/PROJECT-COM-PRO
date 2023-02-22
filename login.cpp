a212dd72d13f5e5ca9a47855a1585d0606b57c30
PP <110951565+PP2546@users.noreply.github.com>
2023-02-22 15:46:01 +0700

    Update login.cpp
    Signed-off-by: PP <110951565+PP2546@users.noreply.github.com>

--- login.cpp
+++ login.cpp
@@ -1,1 +1,58 @@
+void bookstore::Login()
+{
+    int come;
+    string name , password , n , pw;
+    cout << "\n";
+    cout << "\t----------- Login ----------\n";
+    cout << "\tEnter is your username : ";
+    cin >> name;
+    cout << "\tEnter is your password : ";
+    cin >> password;
 
+    ifstream source;
+    source.open("records.txt",ios::app);
+    
+    while(source >> n >> pw)
+    {
+        if(n == name && pw == password)
+        {
+            come = 1;
+        }
+    }
+
+    source.close();
+
+    if(come == 1)
+    {
+        cout << "\nLogin successfaul ++\n\n";
+        cout << "Hello , " << name << " What do you want to do?\n";
+        cin.get();
+    }
+    else
+    {
+        cout << "\nLogin failed , please try again.\n ";
+        cin.get(); 
+        Login();     
+    }
+}
+
+// Function Register
+void bookstore::Register()
+{
+    string name , password;
+    //กรอกชื่อและรหัสเพื่อสมัคร
+    cout << "\n";
+    cout << "\t--------- Register ---------\n";
+    cout << "\tWhat is your username? : ";
+    cin >> name;
+    
+    cout << "\tWhat is your password? : ";
+    cin >> password;
+    
+    ofstream dest;
+    dest.open("records.txt",ios::app);
+    dest << name << " " << password << endl;
+    
+    cout << "\nSuccessful register ++ \n";
+    // สมัครเสร็จเเล้ว จะไปที่หน้า first page
+  
